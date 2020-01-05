fcf3b3b8ea490bc079cfbc0fd9c8c399fdef0a9a
Daniel Zielinski <zielinski.daniel1988@gmail.com>
2020-01-05 15:19:26 +0100

    small edits

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -17,10 +17,11 @@ GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
 GH_BRANCH=master
 
 # Deploy the CloudFormation template
-echo -e "\n\n=========== Deploying main.yml ==========="
+echo -e "\n\n=========== Deploying setup.yml ==========="
 
 aws cloudformation deploy --region $REGION --profile $CLI_PROFILE --stack-name $STACK_NAME-setup --template-file setup.yml --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM --parameter-overrides "CodePipelineBucket=$CODEPIPELINE_BUCKET"
 
+echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy --region $REGION --profile $CLI_PROFILE --stack-name $STACK_NAME --template-file main.yml --no-fail-on-empty-changeset --capabilities CAPABILITY_NAMED_IAM --parameter-overrides "EC2InstanceType=$EC2_INSTANCE_TYPE" "GitHubOwner=$GH_OWNER" "GitHubRepo=$GH_REPO" "GitHubBranch=$GH_BRANCH" "GitHubPersonalAccessToken=$GH_ACCESS_TOKEN" "CodePipelineBucket=$CODEPIPELINE_BUCKET"
 
 # If the deploy succeeded, show the DNS name of the created instance
